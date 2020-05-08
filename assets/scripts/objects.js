c7252aa81f77220fe9e3252f40077e882ffb7fed
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-05-08 17:54:55 +0300

    Create a rendering function to index.html view page

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -4,6 +4,34 @@ const searchBtn = document.getElementById('search-btn');
 const movies = [];
 
 
+const renderMovies = (filter = '') => {
+    const movieList = document.getElementById('movie-list');
+
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+
+    movieList.innerHTML = '';
+
+    const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+
+    for (movie of filteredMovies) {
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + ' - ';
+
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text + `${key}: ${movie.info[key]}`;
+            }
+        }
+
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    }
+}
 
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
